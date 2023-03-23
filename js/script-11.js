8fc92eaac22b418e1385d8efb303984b5e9e115a
Falconoff <sokolovserega1982@gmail.com>
2023-03-23 23:20:34 +0200

    theory of  Massives chapter

--- js/script-11.js
+++ js/script-11.js
@@ -1,1 +1,37 @@
 "use strict";
+
+let fruits = ["Яблоко", "Апельсин", "Слива"];
+
+console.log(fruits[2]); // Слива
+fruits[2] = "Груша"; // теперь ["Яблоко", "Апельсин", "Груша"]
+fruits[3] = "Лимон"; // теперь ["Яблоко", "Апельсин", "Груша", "Лимон"]
+console.log("fruits.length=", fruits.length); // 3
+
+fruits[6] = "Слива";
+console.log(fruits); // теперь ['Яблоко', 'Апельсин', 'Груша', 'Лимон', empty, empty, 'Слива']
+console.log("fruits.length=", fruits.length); // 7
+
+fruits = ["Яблоко", "Апельсин", "Груша", "Лимон", "Слива"];
+
+//  .at
+console.log("====== fruits.at() ======");
+console.log(fruits.at(1)); // Апельсин
+console.log(fruits.at(-1)); // Слива
+
+// pop & push
+console.log("====== pop & push ======");
+
+console.log(fruits); //  ['Яблоко', 'Апельсин', 'Груша', 'Лимон', empty × 2, 'Слива']
+console.log(fruits.pop()); // 'Слива'
+console.log(fruits); // ['Яблоко', 'Апельсин', 'Груша', 'Лимон', empty × 2]
+
+fruits.push("Kiwi");
+console.log(fruits); // ['Яблоко', 'Апельсин', 'Груша', 'Лимон', empty × 2, 'Kiwi']
+
+// shift & unshift
+console.log("====== shift & unshift ======");
+
+fruits.shift();
+console.log(fruits); // ['Апельсин', 'Груша', 'Лимон', empty × 2, 'Kiwi']
+fruits.unshift("Banana 1", "Banana 2");
+console.log(fruits); // ['Banana 1', 'Banana 2', 'Апельсин', 'Груша', 'Лимон', empty × 2, 'Kiwi']
