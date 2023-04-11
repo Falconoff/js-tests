ea61ff581b7629d2f2b4283dc1e939b5ff6bf3b9
Falconoff <sokolovserega1982@gmail.com>
2023-04-11 16:15:23 +0300

    weakmap-weakset

--- js/script-14.js
+++ js/script-14.js
@@ -20,3 +20,21 @@ let values = [
 ];
 
 console.log(unique(values)); // Hare,Krishna,:-O
+
+console.log(" ---------------- Task 3 ---------------- ");
+// Перебираемые ключи
+// Мы хотели бы получить массив ключей map.keys() в переменную и далее работать с ними, например, применить метод .push.
+// Это потому что map.keys() возвращает итерируемый объект, а не массив.
+// Мы можем конвертировать его в массив с помощью Array.from:
+let map = new Map();
+
+map.set("name", "John");
+
+let keys = Array.from(map.keys());
+
+console.log(map);
+console.log(Object.entries(map));
+
+// Error: keys.push is not a function
+// Ошибка: keys.push -- это не функция
+keys.push("more");
