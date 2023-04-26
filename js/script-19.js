68d0818faa255f9faf3027f2b156327117d23cbb
Falconoff <sokolovserega1982@gmail.com>
2023-04-26 22:46:57 +0300

    JSON_continue

--- js/script-19.js
+++ js/script-19.js
@@ -17,3 +17,28 @@ console.log(typeof json); // мы получили строку - string
 
 console.log(json); // выведет объект в формате JSON
 // {"name":"John","age":30,"isAdmin":false,"courses":["html","css","js"],"wife":null}
+
+console.log("-------------------");
+
+// число в JSON остаётся числом
+console.log(JSON.stringify(1)); // 1
+
+// строка в JSON по-прежнему остаётся строкой, но в двойных кавычках
+console.log(JSON.stringify("test")); // "test"
+
+console.log(JSON.stringify(true)); // true
+
+console.log(JSON.stringify([1, 2, 3])); // [1,2,3]
+
+console.log("-------------------");
+
+let user = {
+  sayHi() {
+    // будет пропущено
+    console.log("Hello");
+  },
+  [Symbol("id")]: 123, // также будет пропущено
+  something: undefined, // как и это - пропущено
+};
+
+console.log(JSON.stringify(user)); // {} (пустой объект)
