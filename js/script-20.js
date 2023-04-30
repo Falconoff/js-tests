73351a8a82742b06d93534e07e29b80c2c5bb9fb
Falconoff <sokolovserega1982@gmail.com>
2023-04-30 23:47:10 +0300

    Callback. Example 1

--- js/script-20.js
+++ js/script-20.js
@@ -1,3 +1,22 @@
 "use strict";
 
 console.log("========== Callback ===============");
+
+// расширяемая ф-ция математич. операции
+
+const doMath = function (a, b, callback) {
+  const result = callback(a, b);
+
+  console.log(result);
+};
+
+const add = function (x, y) {
+  return x + y;
+};
+
+const sub = function (x, y) {
+  return x - y;
+};
+
+doMath(2, 3, add); // 5
+doMath(6, 2, sub); // 4
