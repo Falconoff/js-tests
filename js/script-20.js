801dd6d6943f408b0dcd25536ec007bb5786e2a0
Falconoff <sokolovserega1982@gmail.com>
2023-05-02 23:29:24 +0300

    Closures

--- js/script-20.js
+++ js/script-20.js
@@ -83,3 +83,49 @@ console.log(rez1);
 
 const rez2 = filter([1, 2, 3, 4, 5, 6, 7, 8], callback1);
 console.log(rez2);
+
+// замыкание
+
+console.log("========== Closure ===============");
+
+// --- Поварёнок
+
+const makeSheff = function (name) {
+  const innerVar = "cooks";
+
+  const makeDish = function (dish) {
+    console.log(`${name} ${innerVar} ${dish}`);
+  };
+
+  return makeDish;
+};
+
+const mango = makeSheff("Mango");
+
+// console.log(mango);
+// console.dir(mango);
+
+mango("tea");
+mango("coffee");
+
+const poly = makeSheff("Poly");
+
+poly("pizza");
+poly("pasta");
+mango("juice");
+
+// --- Округление
+
+const rounder = function (places) {
+  return function (number) {
+    return Number(number.toFixed(places));
+  };
+};
+
+const rounder2 = rounder(2);
+const rounder3 = rounder(3);
+
+console.log(rounder2(1.234456));
+console.log(rounder3(1.234456));
+console.log(rounder2(34.765765));
+console.log(rounder3(34.765765));
