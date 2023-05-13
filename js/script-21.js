9826e4d3a42e4879d93a0098abe7fdff2fdff421
Falconoff <sokolovserega1982@gmail.com>
2023-05-13 23:08:17 +0300

    this_context

--- js/script-21.js
+++ js/script-21.js
@@ -1,6 +1,18 @@
 "use strict";
 
-console.log("========== Context ===============");
+// console.log("========== Контекст исполнения (this) ===============");
+
+console.log("---------- this в глобальной области видимости -----------");
+
+function fn() {
+  console.log("this в глобальной области видимости -", this);
+}
+
+fn(); // window без "use strict" и undefined с "use strict"
+
+// -------------------------------------------------------------
+
+console.log("---------- this в методе объекта -----------");
 
 // Петя бежит быстро, потому что Петя пытается поймать поезд.
 const petya = {
@@ -24,16 +36,20 @@ petya.showThis(); // {name: 'Petya', showName: ƒ, showThis: ƒ}
 
 const bookShelf = {
   authors: ["Бернард Корнуэлл", "Роберт Шекли"],
+
   getAuthors() {
     return this.authors;
   },
+
   addAuthor(authorName) {
     this.authors.push(authorName);
   },
 };
 
 console.log(bookShelf.getAuthors()); // ["Бернард Корнуэлл", "Роберт Шекли"]
+
 bookShelf.addAuthor("Ли Танит");
+
 console.log(bookShelf.getAuthors()); // ["Бернард Корнуэлл", "Роберт Шекли", "Ли Танит"]
 
 // ================================================================================
@@ -48,8 +64,7 @@ function showThis() {
 }
 
 // Вызываем в глобальном контексте
-showThis();
-// this in showThis: Window
+showThis(); // this in showThis: Window
 
 const user = {
   name: "Mango",
@@ -62,7 +77,7 @@ const anotherUser = {
 };
 
 /*
- * Записываем ссылку на функцию в свойство объекта
+ * Записываем ссылку на функцию в свойство объекта user
  * Обратите внимание, что это не вызов - нет ()
  */
 user.showContext = showThis;
@@ -81,14 +96,30 @@ anotherUser.showContext(); // this in showThis:  {name: 'Poly', age: 19, showCon
 
 // =====================================================================================
 
-// =====================================================================================
+// ================ this в callback-функциях ============
 
-// =====================================================================================
+console.log("---------- this в функциях обратного вызова -----------");
 
-// =====================================================================================
+const hotel = {
+  name: "Resort Hotel",
+  showThis() {
+    console.log("this from hotel -", this);
+  },
+};
 
-/*
-// this в callback-функциях
+const fnWithCallback = function (callback) {
+  /*
+   * Во время вызова fnWithCallback, callback будет ссылкой на функцию showThis объекта hotel.
+   * Ее вызов происходит в глобальном контексте, про hotel она ничего не знает.
+   * Соответственно this не будет ссылаться на hotel
+   */
+  callback();
+};
+
+// Передается ссылка на функцию а нее ее вызов
+fnWithCallback(hotel.showThis); // window или undefined
+
+// -------------------------------------------------------------
 
 // При передаче методов объекта как колбэк-функций, контекст не сохраняется. Колбэк это ссылка на метод, которая присваивается как значение параметра, вызываемого без объекта.
 
@@ -105,37 +136,40 @@ function makeMessage(callback) {
   console.log(`Обрабатываем заявку от ${callback()}.`);
 }
 
-makeMessage(customer.getFullName); // Будет ошибка при вызове функции
+// makeMessage(customer.getFullName); // Будет ошибка при вызове функции
 
 // Решение этой проблемы рассматривается в секции про метод bind() и методы объекта.
-*/
+
+// =====================================================================================
 
 // this в стрелочных функциях
 
+console.log("---------- this в стрелочных функциях -----------");
+
 // Стрелочные функции не имеют своего this. В отличии от обычных функций, изменить значение this внутри стрелки после её объявления нельзя.
 // Контекст внутри стрелки определяется местом её объявления, а не вызова и ссылается на контекст родительской функции.
-/*
-const showThis = () => {
-  console.log("this in showThis: ", this);
+
+const showThisArrow = () => {
+  console.log("this in showThisArrow: ", this);
 };
 
-showThis(); // this in showThis: window
+showThisArrow(); // this in showThisArrow: window
 
-const user = {
+const user2 = {
   username: "Mango",
 };
-user.showContext = showThis;
+user2.showContext = showThisArrow;
 
-user.showContext(); // this in showThis: window
-*/
+user2.showContext(); // this in showThisArrow: window
 
-/*
-const hotel = {
+// -----------------------------------------------------
+
+/* Пример не практичный, но отлично показывает как работает контекст для стрелок. Значение контекста берется из родительской области видимости. */
+const hotel_2 = {
   username: "Resort hotel",
   showThis() {
     const foo = () => {
-      // Стрелки запоминают контекст во время объявления,
-      // из родительской области видимости
+      // Стрелки запоминают контекст во время объявления, из родительской области видимости
       console.log("this in foo: ", this);
     };
 
@@ -144,10 +178,9 @@ const hotel = {
   },
 };
 
-hotel.showThis();
+hotel_2.showThis();
 // this in foo: {username: 'Resort hotel', showThis: ƒ}
 // this in showThis: {username: 'Resort hotel',showThis: ƒ}
-*/
 
 /**/
 
