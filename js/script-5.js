a5a9ea376f6ed5fadec6d768b7c77d93ce59395e
Falconoff <sokolovserega1982@gmail.com>
2023-10-31 21:22:13 +0200

    some adds to logical operations

--- js/script-5.js
+++ js/script-5.js
@@ -1,6 +1,6 @@
 // =========== ИЛИ ==============
 
-// ИЛИ - || возвращает первое истинное значение или последнее, если такое значение не найдено.
+// ИЛИ - || возвращает первое истинное значение или, если такое значение не найдено, последнее ложное.
 
 let hour = 12;
 let isWeekend = true;
@@ -8,6 +8,17 @@ let isWeekend = true;
 if (hour < 10 || hour > 18 || isWeekend) {
   console.log("Офис закрыт."); // это выходной
 }
+
+// ----
+const truthy1 = 123;
+const truthy2 = [1, 2, 3];
+const truthy3 = "abc";
+const falsy1 = null;
+const falsy2 = "";
+const falsy3 = 0;
+
+console.log(truthy1 || truthy2 || truthy3); // 123
+console.log(falsy1 || falsy2 || falsy3); // 0
 
 // =========== И ==============
 
@@ -20,11 +31,17 @@ if (hours == 12 && minutes == 30) {
   console.log("The time is 12:30");
 }
 
+// ----
+console.log(truthy1 && truthy2 && truthy3); // abc
+console.log(falsy1 && falsy2 && falsy3); // null
+
 // =========== НЕ ==============
 
 // Сначала приводит аргумент к логическому типу true/false. Затем возвращает противоположное значение.
 
-// двойное НЕ !! используют для преобразования значений к логическому типу:
+console.log(!"non-empty string"); // false
+console.log(!null); // true
 
+// двойное НЕ !! используют для преобразования значений к логическому типу:
 console.log(!!"non-empty string"); // true
 console.log(!!null); // false
