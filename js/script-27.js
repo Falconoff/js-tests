0f102655d0ca606ccccc970aea27da1bf78302e2
Falconoff <sokolovserega1982@gmail.com>
2024-04-16 21:08:11 +0300

    add form

--- js/script-27.js
+++ js/script-27.js
@@ -1,7 +1,7 @@
 const changeBtnEl = document.querySelector(".change-btn");
 // console.log(changeBtnEl);
 
-const sectionEl = document.querySelector(".section");
+const sectionOneEl = document.querySelector(".section-1");
 
 const navListEl = document.querySelector(".nav__list");
 const firstNavItemEl = navListEl.firstElementChild;
@@ -34,7 +34,7 @@ const onChangeBtnClick = function () {
   firstNavItemEl.style.color = "yellow";
   firstNavLink.style.color = "green";
 
-  sectionEl.appendChild(newMessage);
+  sectionOneEl.appendChild(newMessage);
 
   navListEl.insertAdjacentHTML("beforeend", newListItem);
 };
@@ -44,3 +44,71 @@ const onChangeBtnClick = function () {
 // });
 
 changeBtnEl.addEventListener("click", onChangeBtnClick);
+
+// ============================================================
+
+// Паттерн "Объект ссылок"
+const refs = {
+  form: document.querySelector(".form"),
+  input: document.querySelector(".js-input"),
+  licenseCheckbox: document.querySelector(".js-license"),
+  userName: document.querySelector(".js-license + span"),
+  btn: document.querySelector(".js-btn"),
+};
+
+refs.form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  // доступ к одному из значений, заполненных в форме (почта)
+  // console.dir(event.currentTarget.elements.email.value);
+
+  // FormData - собирает в одну итерируемую сущность все данные из формы
+  const formData = new FormData(event.currentTarget);
+
+  formData.forEach((value, key) => {
+    console.log("key:", key);
+    console.log("value:", value);
+  });
+}
+
+// refs.input.addEventListener("focus", onInputFocus);
+// refs.input.addEventListener("blur", onInputBlur);
+refs.input.addEventListener("input", onInputChange);
+refs.licenseCheckbox.addEventListener(
+  "change",
+  onLicenseChange,
+);
+
+function onInputFocus() {
+  console.log(
+    "Input 'user-name' получил фокус - событие focus",
+  );
+}
+function onInputBlur() {
+  console.log(
+    "Input 'user-name' потерял фокус - событие blur",
+  );
+}
+
+function onInputChange(event) {
+  console.log(event.currentTarget.value);
+  refs.userName.textContent = event.currentTarget.value;
+}
+
+function onLicenseChange(event) {
+  // console.log(event.currentTarget.checked);
+  refs.btn.disabled = !event.currentTarget.checked;
+}
+
+// ========================================================
+
+// ====== События клавиатуры: keypress, keydown, keyup ======
+
+window.addEventListener("keypress", onKeyPress);
+
+function onKeyPress(event) {
+  console.log("event.key:", event.key);
+  console.log("event.code:", event.code);
+}
